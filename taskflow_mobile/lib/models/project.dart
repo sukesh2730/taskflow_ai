e6c7d57721fbf8b70573fc840c79f6cf243549f8
sukesh2730 <sukeshkavin@gmail.com>
2026-01-09 15:18:04 +0530

    Add fromMap and toMap methods to Project model

--- taskflow_mobile/lib/models/project.dart
+++ taskflow_mobile/lib/models/project.dart
@@ -12,4 +12,21 @@ class Project {
       description: json['description'],
     );
   }
+
+  factory Project.fromMap(Map<String, dynamic> map) {
+    int parseInt(dynamic v) => v is int ? v : (v != null ? int.parse(v.toString()) : 0);
+    return Project(
+      id: map['id'] is int ? map['id'] as int : parseInt(map['id']),
+      name: map['name']?.toString() ?? '',
+      description: map['description']?.toString(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+    };
+  }
 }
